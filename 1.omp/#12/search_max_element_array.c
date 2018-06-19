d339f4825aecb0212e0d04d4c3da27b1571ab771
Sergio Kulyk <sergiokulyk98@gmail.com>
2018-06-19 09:10:34 +0300

    1.5 Add programs.

--- 1.omp/#12/search_max_element_array.c
+++ 1.omp/#12/search_max_element_array.c
@@ -2,3 +2,14 @@
 // Created by sergio on 17.06.18.
 //
 
+int max(int A[], int i, int j)
+{
+    int idx;
+    int max_val; /* = 0 not needed according to Jim Cownie comment */
+
+#pragma omp parallel for reduction(max:max_val)
+    for (idx = i; idx < j; idx++)
+        max_val = max_val > A[idx] ? max_val : A[idx];
+
+    return max_val;
+}
